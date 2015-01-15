845ab1165fb90c552d71c9e62d8fa8a84ca1e385
Craig Griffiths <craig.griffiths@codethink.co.uk>
2015-01-15 12:01:55 +0000

    Change back to using subproccess call, server will require root but this is OK for POC

--- tbdiffWrapper.py
+++ tbdiffWrapper.py
@@ -5,20 +5,19 @@ Created on 28 Nov 2014
 @copyright: 2014 Codethink Ltd. All rights reserved.
 '''
 
+from subprocess import call
+
 
 def tbdiffCreate(patchDir, sourceDir, targetDir):
-    print ("Please run: tbdiff-create " + patchDir + " "
-                                        + sourceDir + " "
-                                        + targetDir)
-    raw_input("Press Enter when the command completes...")
+    return call(["tbdiff-create", patchDir, sourceDir, targetDir])
+
 
 def mount(imagePath, mountPath):
-    print ("Please run: mkdir " + mountPath)
-    raw_input("Press Enter when the command completes...")
+    print ("making directory")
+    call(['mkdir', mountPath])
+    print ("calling mount")
+    return call(["mount", imagePath, mountPath])
 
-    print ("Please run: mount " + imagePath + " " + mountPath)
-    raw_input("Press Enter when the command completes...")
 
 def unmount(mountPath):
-    print ("Please run: umount " + mountPath)
-    raw_input("Press Enter when the command completes...")
+    return call(["umount", mountPath])
